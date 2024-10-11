aa74d1beb913e1d11869b926fce28282579e2c52
Sumögåsbord <61753952+Sumogasbord@users.noreply.github.com>
2024-10-11 23:17:17 +0900

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,25 +2,25 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '11c6333338278086b70bf4b4af41b0c1',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'RooLone Magazine',
+  domain: 'magazine.roolone.me',
+  author: 'Sumögåsbord',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'This is a test.',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
-  // mastodon: '#', // optional mastodon profile URL, provides link verification
+  // twitter: '#',
+  github: 'sumogasbord',
+  // linkedin: '@',
+  mastodon: 'https://vandrare.page/@sumogasbord', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
