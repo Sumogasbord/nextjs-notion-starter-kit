2820db257a14a7c4cb3a18787ed5adef35ab11b6
sumogasbord <sumogasbord@gmail.com>
2025-10-12 09:01:10 +0900

    My Setting

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '18d633333827804c845ef6e7a6e95b45',
+  rootNotionPageId: '28963333382781b98f25d70b72976beb',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
